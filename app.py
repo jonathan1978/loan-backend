edabc24195051044f6fd0d98797961edb2b47774
Jonathan Lawerh <jonathan@cloudport.tech>
2020-12-07 16:49:39 +0530

    update

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ api.add_resource(CustomerList, '/')
 api.add_resource(CustomerDetails, '/customers/<customer_name>')
 
 if __name__ == "__main__":
-    app.run(debug=True,port=PORT)
+    app.run(debug=True,port=PORT,host='0.0.0.0')
